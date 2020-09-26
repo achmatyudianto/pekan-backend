4ccfc1a7a0bc8f0155b2bc1a2cc33603c640a67f
yudi <achmatyudianto@gmail.com>
2020-09-26 17:08:36 +0700

    mbiodo

---
+++ app/Http/Livewire/Auth/Register.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Livewire\Auth;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Component;
+use App\User;
+
+class Register extends Component
+{
+	public $name;
+    public $email;
+    public $password;
+    public $password_confirmation;
+    
+    /**
+     * store
+     *
+     * @return void
+     */
+    public function store()
+    {
+        $this->validate([
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|confirmed|min:8'
+        ]);
+
+        $user = User::create([
+            'name'      => $this->name,
+            'email'     => $this->email,
+            'api_token'	=> Str::random(60),
+			'password'	=> Hash::make($this->password),
+        ]);
+
+        if($user) {
+            session()->flash('success', 'Register Berhasil!.');
+            return redirect()->route('auth.login');
+        }
+    }
+
+    /**
+     * render
+     *
+     * @return void
+     */
+    public function render()
+    {
+        return view('livewire.auth.register');
+    }
+}
