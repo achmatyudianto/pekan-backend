2e9928a361e3e1e1445febf4403f6c7a77fc1675
yudi <achmatyudianto@gmail.com>
2020-10-23 17:09:50 +0700

    Fixed leanding page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "<center>Pekan (Pengeluaran, Pemasukan)</center>";
 });
 
 Route::group(['middleware' => 'guest'], function(){
